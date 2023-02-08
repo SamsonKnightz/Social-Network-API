8515551916853c54b85b3ac4bd2cd37c6513d34e
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2023-02-07 22:24:45 -0500

    should be able to recieve a single thought

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+  getThought,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  deleteReaction,
+  createReaction
+} = require('../../controllers/thoughtController.js');
+
+// /api/courses
+router.route('/').get(getThought).post(createThought);
+
+// /api/courses/:courseId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
