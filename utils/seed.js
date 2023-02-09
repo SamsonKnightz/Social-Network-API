96aec5679c7a3a54275d0e276b562400594d08e9
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2023-02-08 22:15:30 -0500

    working on data and seeds files to seed correct data

--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,6 @@
 const connection = require('../config/connection');
-const { User, Video } = require('../models');
-const { getRandomName, getRandomVideos } = require('./data');
+const { User, Thought } = require('../models');
+const { getRandomName, getRandomReaction } = require('./data');
 
 connection.on('error', (err) => err);
 
@@ -8,7 +8,7 @@ connection.once('open', async () => {
   console.log('connected');
 
   const users = [];
-  const videos = getRandomVideos(10);
+  const possibleReaction = getRandomReaction(10);
 
   for (let i = 0; i < 20; i++) {
     const fullName = getRandomName();
@@ -18,14 +18,15 @@ connection.once('open', async () => {
     users.push({
       first,
       last,
-      age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
+      possibleReaction,
     });
   }
 
 
+
   // loop through the saved videos, for each video we need to generate a video response and insert the video responses
   console.table(users);
-  console.table(videos);
+  console.table(possibleReaction);
   console.info('Seeding complete! 🌱');
   process.exit(0);
 });
